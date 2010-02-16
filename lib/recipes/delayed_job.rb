3caee627ae6290fc0a3e5e28f56b2f2d01f78718
John Paul Narowski <jnarowski@gmail.com>
2010-02-16 15:52:53 -0500

    fixed delayed_job script

--- lib/recipes/delayed_job.rb
+++ lib/recipes/delayed_job.rb
@@ -16,12 +16,12 @@ Capistrano::Configuration.instance(:must_exist).load do
 
     desc "Start the delayed_job process"
     task :start, :roles => :app do
-      run "cd #{current_path}; #{sudo} #{rails_env} script/delayed_job start"
+      run "cd #{current_path}; #{sudo} RAILS_ENV=#{rails_env} script/delayed_job start"
     end
 
     desc "Stop the delayed_job process"
     task :stop, :roles => :app do
-      run "cd #{current_path}; #{sudo} #{rails_env} script/delayed_job stop"
+      run "cd #{current_path}; #{sudo} RAILS_ENV=#{rails_env} script/delayed_job stop"
     end
 
   end
